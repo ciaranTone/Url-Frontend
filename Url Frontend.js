2ed0bae9bfb49db3918055965b420a71fd16dcc1
ciaranTone <ciaran.tone@Zinkworks.com>
2025-02-18 10:03:19 +0000

    Url frontend

---
+++ Url Frontend.js
@@ -0,0 +1,54 @@
+// src/components/UrlList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../App.css';
+
+const GetUrls = () => {
+    const [urls, setUrls] = useState([]);// State to hold the list of URLs
+
+    // useEffect hook to fetch URLs when the component mounts
+    useEffect(() => {
+        const fetchUrls = async () => {
+            try{
+                // Make a GET request to fetch all URLs
+                const response = await axios.get('http://localhost:8080/url-shortener'); 
+                setUrls(response.data);
+            } catch (error) {
+                console.error('Error fetching URLs', error);
+            }
+        };
+        fetchUrls();
+    },[]);
+
+    const deleteUrl = async (id) => {
+        try {
+            
+            await axios.delete(`http://localhost:8080/url-shortener/delete/${id}`);
+
+            setUrls(urls.filter(url => url.id !== id));
+        } catch (error) {
+            console.error('Error deleting URL', error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="App-header">
+                <h2>Shortened URLs</h2>
+            </div>
+            <ul>
+                {urls.map((url) => (
+                    <li key={url.id}>
+                        <a href={url.originalUrl} target="_blank" rel="noopener noreferrer" className="Shortened-Url">
+                            {url.shortenedUrl}
+                        </a>
+                        <button onClick={() => deleteUrl(url.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+    
+};
+
+export default GetUrls;
